206d41fdf51b58409c2d282a3f3ddd723e619d64
Unknown <ziemannjakub@gmail.com>
2017-05-17 22:48:55 +0200

    Changed nav bars

--- CrossfitStocznia/MyReservationsViewController.swift
+++ CrossfitStocznia/MyReservationsViewController.swift
@@ -20,7 +20,7 @@ class MyReservationsViewController: UIViewController {
         
         super.viewDidLoad()
         
-        
+        self.edgesForExtendedLayout = []
         
     }
     
--- CrossfitStocznia/SearchViewController.swift
+++ CrossfitStocznia/SearchViewController.swift
@@ -33,7 +33,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.contentInset = UIEdgeInsets(top: 44, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 88, left: 0, bottom: 0, right: 0)
         
         
         tableView.rowHeight = 80
